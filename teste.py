555b9507d754d7a73f593d09d64d62d055920fbe
alex <alex.bertolla@gmail.com>
2019-02-15 16:22:04 -0200

    NOVA IMAGEM INCLUIDA E ALTERACAO DO CODIGO

--- teste.py
+++ teste.py
@@ -1,3 +1,9 @@
 import cv2 as cv
 
+img_path = 'C:/Users/alex/Dropbox/repositorio_git/curso_python/imagem_7.jpg'
+
+img = cv.imread(img_path)
+cv.imshow('imagem', img)
+cv.waitKey(0)
+
 print('FIM')
