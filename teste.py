d2e055bb6ab801ea9626ee7a70cd4b65220b6629
alex.bertolla <alex.bertolla@3106539-000.instrumentacao.local>
2019-02-15 16:26:56 -0200

    caminho script

--- teste.py
+++ teste.py
@@ -1,9 +1,13 @@
 import cv2 as cv
+import _version
+import _locale
 
-img_path = 'C:/Users/alex/Dropbox/repositorio_git/curso_python/imagem_7.jpg'
+print(__file__)
 
-img = cv.imread(img_path)
-cv.imshow('imagem', img)
-cv.waitKey(0)
+#img_path = 'C:/Users/alex/Dropbox/repositorio_git/curso_python/imagem_7.jpg'
+
+#img = cv.imread(img_path)
+#cv.imshow('imagem', img)
+#cv.waitKey(0)
 
 print('FIM')
