918d3cfb757b286a23407e6a3c885d036ff23835
alex <alex@megaware>
2019-02-15 20:14:25 -0200

    Commit instalação laptop

--- teste.py
+++ teste.py
@@ -1,13 +1,4 @@
-import cv2 as cv
-import _version
-import _locale
-
 print(__file__)
 
-#img_path = 'C:/Users/alex/Dropbox/repositorio_git/curso_python/imagem_7.jpg'
-
-#img = cv.imread(img_path)
-#cv.imshow('imagem', img)
-#cv.waitKey(0)
 
 print('FIM')
